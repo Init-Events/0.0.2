5980394910297ebfb9f89eb5f78710b2ae3e8b8c
David Westwood <davidw3214@gmail.com>
2023-09-05 18:46:20 -0600

    Added textToEmotion module for project

---
+++ src/textToEmotion.py
@@ -0,0 +1,20 @@
+import text2emotion as te
+import nltk
+
+text = "I love coding and it makes me feel so happy!"
+
+class Emotion:
+
+  def __init__(self, text):
+    self.text = text
+    self.emo = te.get_emotion(text)
+
+  def setText(self, text):
+    self.text = text
+    self.emo = te.get_emotion(text)
+
+  def getEmotions(self):
+    return self.emo
+
+  def getTopEmotion(self):
+    return max(self.emo, key=self.emo.get)
